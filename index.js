ac94617398ac5085e0b61ba38588ce9aa4f40ed2
Caleb <Tigers1025@gmail.com>
2022-08-09 22:21:23 -0400

    commit tree fb8ab3baf7170097ba9198a2238fd45579ac8f98

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const deleteMain = document.getElementById('main')
+deleteMain.remove();
+
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = 'Caleb is the champion'
